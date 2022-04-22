4596f9b63c15ac2b6217682d95bad98ca8ddd6a3
siam666-shell <abdullahsiam009@gmail.com>
2022-04-23 02:28:25 +0600

    redux basics

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,65 @@ import './App.css';
 // component - dispatch action creator - real action - reducer - store
 //store - component access
 
+// store
+// reducer
+// action creator
+// action
+
+// action
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+import { legacy_createStore as createStore } from 'redux';
+
+const initialState = {
+  value: 0,
+};
+
+// reducer
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        value: state.value + 1,
+      };
+    case DECREMENT:
+      return {
+        value: state.value - 1,
+      };
+    case RESET:
+      return {
+        value: 0,
+      };
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(countReducer);
+
+// action creator
+
+const increment = () => {
+  return { type: INCREMENT };
+};
+const decrement = () => {
+  return { type: DECREMENT };
+};
+const reset = () => {
+  return { type: RESET };
+};
+
+store.subscribe(() => console.log(store.getState()));
+
+// raising event or action
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(reset());
+
 function App() {
   return (
     <>
