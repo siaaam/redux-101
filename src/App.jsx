cb77a18de97b6cc59814df239724757de8dc4ff1
siam666-shell <abdullahsiam009@gmail.com>
2022-04-23 01:57:02 +0600

    redux flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
 
+// flow
+
+// store - from where you manage data
+// reducer - function that manipulate or result ultimate data
+// dispatching(raising) action  or event - click,hover,scroll
+// action creator - function that create a action or event
+
+// component - dispatch action creator - real action - reducer - store
+//store - component access
+
 function App() {
-  return <h1>redux 101</h1>;
+  return (
+    <>
+      <p>10</p>
+      <button>increment</button>
+      <button>decrement</button>
+    </>
+  );
 }
 
 export default App;
